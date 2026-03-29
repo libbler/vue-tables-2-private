90eb004e6e434c2128e274a2ea5e8a51139155d2
Alex <67736692+ni-alex@users.noreply.github.com>
2026-03-29 13:14:41 +0700

    document emitted events

--- compiled/helpers/emitted-events.js
+++ compiled/helpers/emitted-events.js
@@ -4,4 +4,4 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _default = exports["default"] = ['loading', 'loaded', 'intersect', 'pagination', 'filter', 'sorted', 'select', 'row-click'];
+var _default = exports["default"] = ['loading', 'loaded', 'error', 'limit', 'intersect', 'pagination', 'filter', 'sorted', 'select', 'row-click'];
